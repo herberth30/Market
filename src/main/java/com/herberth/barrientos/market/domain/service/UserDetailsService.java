74fd8438525ef0409effd973c48e15b975a6d001
Herberth Barrientos <herberthbarrientos30@gmail.com>
2022-02-18 21:52:54 -0600

    Se utilizo Spring Security para la seguridad

---
+++ src/main/java/com/herberth/barrientos/market/domain/service/UserDetailsService.java
@@ -0,0 +1,17 @@
+package com.herberth.barrientos.market.domain.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class  UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("herberth","{noop}contra",
+                new ArrayList<>());
+    }
+}
